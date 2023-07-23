402780555f3c0db32331fdb2ffa517c729d33264
rnldbss <ronaldbessam@gmail.com>
2023-07-23 19:01:43 -0300

    First Approach to Snap to Section failed.

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,8 +1,6 @@
 import { useLocale } from "next-intl";
 import { notFound } from "next/navigation";
 
-import { NextIntlClientProvider } from "next-intl";
-
 import "./globals.css";
 import { Work_Sans } from "next/font/google";
 import { Nav } from "./components/nav";
@@ -13,39 +11,25 @@ import { Ball } from "./components/ball";
 
 const workSans = Work_Sans({ subsets: ["latin"] });
 
-export function generateStaticParams() {
-  return [{ locale: "en" }, { locale: "pt" }];
-}
+export default function LocaleLayout({ children, params }) {
+  const locale = useLocale();
 
-export default async function LocaleLayout({ children, params: { locale } }) {
-  let messages;
-  try {
-    messages = (await import(`../../messages/${locale}.json`)).default;
-  } catch (error) {
+  if (params.locale !== locale) {
     notFound();
   }
-
-  // export default function LocaleLayout({ children, params }) {
-  //   const locale = useLocale();
-
-  //   if (params.locale !== locale) {
-  //     notFound();
-  //   }
 
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className=" dark:bg-gray-950 dark:text-white">
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <Providers>
-            <main className={`${workSans.className}`}>
-              <Nav />
-              <MailMe />
-              <Social />
-              {children}
-              <Ball />
-            </main>
-          </Providers>
-        </NextIntlClientProvider>
+        <Providers>
+          <main className={`${workSans.className}`}>
+            <Nav />
+            <MailMe />
+            <Social />
+            {children}
+            <Ball />
+          </main>
+        </Providers>
       </body>
     </html>
   );
