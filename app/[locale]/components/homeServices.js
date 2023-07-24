8e73313e8aff9ae79a61b31d87a7b3ed6f26d9bf
rnldbss <ronaldbessam@gmail.com>
2023-07-24 19:16:20 -0300

    Home Services

--- app/[locale]/components/homeServices.js
+++ app/[locale]/components/homeServices.js
@@ -25,9 +25,18 @@ export function Services({ secTitle, secBodyOne }) {
             start: "center center",
             endTrigger: "#endTrigger",
             end: "center center ",
-            markers: true,
             pinSpacing: false,
           },
+        });
+        gsap.from("#purpleLine", {
+          scrollTrigger: {
+            trigger: "#purpleLine",
+            start: "top center",
+            markers: true,
+          },
+          height: 0,
+          duration: 2,
+          ease: "power2.out",
         });
       },
     );
@@ -37,20 +46,27 @@ export function Services({ secTitle, secBodyOne }) {
   return (
     <section
       ref={servicesRef}
-      className="flex min-h-[100dvh] place-items-center "
+      className=" flex min-h-[100dvh]  place-items-center pt-28"
     >
       <div
         id="servicesWrapper"
         className="relative  z-20  mb-20  grid  grid-cols-12 gap-1 "
       >
-        <h2
+        <div
           id="servicesTitle"
-          className=" col-start-2 col-end-12 max-h-min self-start break-normal text-5xl font-bold sm:col-start-3 sm:col-end-7 sm:leading-tight lg:col-end-6 lg:text-5xl xl:text-6xl"
+          className="col-start-2 col-end-12 mt-20 flex max-h-min  gap-2 self-start sm:col-start-3 sm:col-end-7  lg:col-end-6 "
         >
-          {secTitle}
-        </h2>
+          <div
+            id="purpleLine"
+            className="hidden bg-violet-600 sm:ml-px sm:inline-block sm:h-full sm:w-2 "
+          ></div>
+          <h2 className=" text-5xl font-bold  lg:text-5xl xl:text-6xl">
+            {secTitle}
+          </h2>
+        </div>
+
         <div className="col-start-2 col-end-12 flex flex-col  sm:col-start-7 sm:col-end-12 lg:col-end-11  xl:col-end-10">
-          <div className="flex flex-col justify-center sm:min-h-[60lvh]">
+          <div className="flex flex-col justify-center text-lg sm:min-h-[60lvh]">
             <p>{secBodyOne}</p>
           </div>
           <div className="flex  flex-col justify-center sm:min-h-[60lvh]">
--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -11,8 +11,10 @@ export default function Index() {
   return (
     <>
       <section className="grid h-[100dvh] grid-cols-12">
-        <div className="relative z-20 col-start-2 col-end-11 flex flex-col gap-4 self-center  sm:col-start-3 md:col-end-10 lg:col-start-2 lg:col-end-8">
-          <h1 className="text-4xl font-semibold">{t("title")}</h1>
+        <div className="relative z-20 col-start-2 col-end-11 flex flex-col gap-4 self-center  sm:col-start-3 md:col-end-10 lg:col-start-2 lg:col-end-10 xl:col-end-8">
+          <h1 className="text-4xl font-semibold sm:text-5xl lg:text-6xl">
+            {t("title")}
+          </h1>
           <p className="text-2xl font-medium">{t("subheading")}</p>
           <ul className="flex gap-3 text-lg underline ">
             <li className="hover:text-slate-300">
@@ -23,7 +25,7 @@ export default function Index() {
             </li>
           </ul>
         </div>
-        <ScrollIndicator />
+        {/* <ScrollIndicator /> */}
       </section>
       <Services secTitle={t("fSecTitle")} secBodyOne={t("fSecBodyOne")} />
       <Services secTitle={t("sSecTitle")} secBodyOne={t("sSecBodyOne")} />
