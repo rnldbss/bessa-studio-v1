8fabbb25931d9cf02fa73a8144b0e5facf7d3f87
rnldbss <ronaldbessam@gmail.com>
2023-07-27 23:04:36 -0300

    works page

--- app/[locale]/works/components/project.js
+++ app/[locale]/works/components/project.js
@@ -29,7 +29,7 @@ export function Project({
       variants={variants}
       initial={inital}
       transition={{ duration: 0.6 }}
-      className={`${tailClass} relative z-20 col-start-2 col-end-12 row-start-1 row-end-1  flex max-w-[1150px] flex-col justify-center gap-4 sm:col-start-3 lg:col-start-3 xl:col-start-3 xl:justify-self-start`}
+      className="relative z-20 col-start-2 col-end-12 row-start-1 row-end-1  flex max-w-[1150px] flex-col justify-center gap-4 pt-12 sm:col-start-3 sm:pt-0 lg:col-start-3 xl:col-start-3 xl:justify-self-start"
     >
       <div className="flex w-full flex-col lg:flex-row-reverse">
         <div className="shadow-lg dark:shadow-slate-600">
@@ -37,27 +37,33 @@ export function Project({
         </div>
 
         <div className="flex flex-col justify-between gap-2  py-4 lg:w-4/5  lg:max-w-2xl xl:px-7 xl:py-7">
-          <h2 className="text-3xl lg:text-4xl">{title}</h2>
-          <h3 className="text-xl lg:text-2xl">{subtitle}</h3>
+          <h2 className="text-3xl font-semibold lg:text-4xl">{title}</h2>
+          <h3 className="text-xl font-medium lg:text-2xl">{subtitle}</h3>
           <p>{body}</p>
           <ul className="flex gap-3">
             <li>
-              <span className="text-sm dark:text-slate-400">{launch}</span>
+              <span className="dark:text- text-sm font-medium text-violet-600">
+                {launch}
+              </span>
               <br />
               {launchDate}
             </li>
             <li>
-              <span className="text-sm dark:text-slate-400">{dev} </span>
+              <span className="dark:text- text-sm font-medium text-violet-600">
+                {dev}{" "}
+              </span>
               <br />
               {devTech}
             </li>
             <li>
-              <span className="text-sm dark:text-slate-400">{category}</span>
+              <span className="dark:text- text-sm font-medium text-violet-600">
+                {category}
+              </span>
               <br />
               {categorySite}
             </li>
           </ul>
-          <a className=" uppercase" href={siteHref} target="_blank">
+          <a className=" font-medium uppercase" href={siteHref} target="_blank">
             {siteName} <BiLinkExternal className="inline text-xl" />
           </a>
         </div>
