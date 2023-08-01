77cfb60316a002469af9271cee0dab006c2d918f
rnldbss <ronaldbessam@gmail.com>
2023-07-31 22:31:10 -0300

    sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,15 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.bessa.dev",
+    },
+    {
+      url: "https://www.bessa.dev/about",
+    },
+    {
+      url: "https://www.bessa.dev/works",
+    },
+  ];
+}
